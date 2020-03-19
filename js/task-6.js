0ca471364b867f42f09c8cb3570d0867d4523b4b
Bogdan <mormul96@ukr.net>
2020-03-19 02:12:56 +0200

    result

--- js/task-6.js
+++ js/task-6.js
@@ -1,1 +1,24 @@
 "use strict";
+
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 }
+];
+
+const calculateTotalPrice = function(allProdcuts, productName) {
+  let result;
+
+  for (const product of products) {
+    if (product["name"] === productName) {
+      result = product["price"] * product["quantity"];
+    }
+  }
+
+  return result;
+};
+
+console.log(calculateTotalPrice(products, "Радар")); // 5200
+
+console.log(calculateTotalPrice(products, "Дроид")); // 2800
